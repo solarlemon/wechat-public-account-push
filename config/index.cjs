015e5ed8517d5916feb9c9c5780c90cd7ebb48b9
solarlemon <shi13378957839@163.com>
2024-12-14 22:35:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,7 +20,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小静',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyrQD666LFcDImkjXMcqNzUs0os8',
+      id: 'oyrQD6-IoGvz4Z5t2oFXZssKdw0o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'EXayWTooiSfhuXKMSN_MagWJ89gfusOkx2KTy9sQ-l4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD

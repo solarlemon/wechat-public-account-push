f4d1f7ded37ca3afa965c9e2f4c13839643facb8
solarlemon <shi13378957839@163.com>
2024-12-14 20:58:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,28 +23,28 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyrQD666LFcDImkjXMcqNzUs0os8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	8qHVJ-j3SLU2pCTy_v4gboW9qGiys_CeBGcEr-W6sPM',
+      useTemplateId: '8qHVJ-j3SLU2pCTy_v4gboW9qGiys_CeBGcEr-W6sPM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-30',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        // {
-        //   type: '*生日', name: '宝贝', year: '2002', date: '06-30',
-        // },
+        {
+          type: '*生日', name: '宝贝', year: '2002', date: '06-30',
+        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '李四', year: '2002', date: '06-30',
         },
-        // {
-        //   type: '节日', name: '相识纪念日', year: '2022', date: '03-18',
-        // },
+        {
+          type: '节日', name: '相识纪念日', year: '2022', date: '03-18',
+        },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2022-03-18' },
         // 结婚纪念日
-        //{ keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],

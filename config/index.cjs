e13414d22070cf37d5398736d791b8cd83eb8f73
solarlemon <shi13378957839@163.com>
2024-12-14 22:23:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyrQD666LFcDImkjXMcqNzUs0os8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'B27lHVzowb7GG4yqhLZNP8-IKgE7mcx8GFJtrWW5KIs',
+      useTemplateId: 'oenEgvo-5IkZicbhPtEmadRjUWqZ12A2634hloWHvbE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-30',
       festivals: [

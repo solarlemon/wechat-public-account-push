f54643206a01a4348f911bfa1dc46db3ac99b6eb
solarlemon <shi13378957839@163.com>
2024-12-14 21:21:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -42,7 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-03-18' },
+        { keyword: 'love_day', date: '2023-03-18' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
